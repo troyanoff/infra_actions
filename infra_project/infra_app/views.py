54b42a7cbb01e9f72c76df555d4e874f212dda01
troyanoff <troyan0v@yandex.ru>
2023-01-25 21:18:57 +0300

    2

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    value = 1234 * 4321 / 54321
+    return HttpResponse(f'У меня получилось! {value}')
 
 
 def second_page(request):
